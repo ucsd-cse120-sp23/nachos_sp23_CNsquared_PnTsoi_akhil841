5a0f9b84fb692adeb7ad343f08477ed317e51fd5
PnTsoi <pntsoi@ucsd.edu>
2023-05-31 18:00:07 -0700

    asdf

--- nachos/vm/VMProcess.java
+++ nachos/vm/VMProcess.java
@@ -123,7 +123,7 @@ public class VMProcess extends UserProcess {
 		switch (cause) {
 		case Processor.exceptionPageFault:
 			int result2 = handlePageFault(processor.readRegister(Processor.regBadVAddr));
-			processor.writeRegister(Processor.regV0, result2);
+			// processor.writeRegister(Processor.regV0, result2);
 			//do not advance PC so program attempts to read address again
 			break;
 		default:
